14ad86af5ba9536cbe13b925e2489abbce007747
PiyushBarde <piyushbarde1@gmail.com>
2021-12-09 19:22:58 +0530

    UPDATED: length comparison computation program

--- LineComparisonComputationProgram.java
+++ LineComparisonComputationProgram.java
@@ -1,5 +1,15 @@
-public class  LineComparisonComputationProgram {
-   public static void main (String[] args) {
-      System.out.println("Welcome to Line Comparison Computation Program");
-   }
+import java.util.Scanner;
+
+public class LineComparisonComputationProgram {
+    public static void main(String[] args) {
+        Scanner Num = new Scanner(System.in);
+        System.out.println("Enter the Co-ordinates of x1 & y1 : ");
+        int x1 = Num.nextInt();
+        int y1 = Num.nextInt();
+        System.out.println("Enter the Co-ordinates of x2 & y2 : ");
+        int x2 = Num.nextInt();
+        int y2 = Num.nextInt();
+        double length = Math.sqrt(( (x2 - x1) * (x2 - x1) ) + ( (y2 - y1) * (y2 - y1)));
+        System.out.println("Length of the Line is : " + length + " Unit" );
+    }
 }
